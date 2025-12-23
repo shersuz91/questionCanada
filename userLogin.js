c581a1f902c98fd694e681b0dbbd4f3b4c6c202c
Sherman <shermanhamorashoca@gmail.com>
2025-12-23 15:56:04 -0700

    Add files via upload

---
+++ userLogin.js
@@ -0,0 +1,11 @@
+var userLoginT=`
+<div>
+    <p>Enter Your Name please:</p>
+    <div>
+        <input type="text" id="userName" name="userName" placeholder="Please enter your name"/>
+        <button id="startBtn">Save</button>
+    </div>
+    <div id="alertName" >please enter your name</div>
+</div>`;    
+
+export default userLoginT;
